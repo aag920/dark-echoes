e0bca4bcc139fd6ce843ada14704386552e47050
Alyssa Gramling <alyssagramling55@gmail.com>
2025-10-07 13:07:45 -0400

    completed workshop

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// StrictMode helps you find common bugs in your components early during development.
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
